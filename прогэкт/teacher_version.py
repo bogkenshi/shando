ed86bd782e572ade878170ca948076e39a459f29
Viacheslav Sukhenko <slavichmy@gmail.com>
2023-03-05 16:24:42 +0200

    edit teacher version

--- прогэкт/teacher_version.py
+++ прогэкт/teacher_version.py
@@ -21,8 +21,11 @@ def send_welcome(message):
 #перше/подальші числа
 @bot.message_handler()
 def process_num1_step(message):
-    answer = f"{message.text} = {eval(message.text)}"
-    bot.reply_to(message, answer)
+    # answer = f"{message.text} = {eval(message.text)}"
+    if message.text == 'Привіт':
+        bot.reply_to(message, "Привіт, як справи?")
+    elif message.text == 'Все добре':
+        bot.reply_to(message, "Вослухай цю музику: https://www.youtube.com/watch?v=EH1I-8KyI9Y")
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
